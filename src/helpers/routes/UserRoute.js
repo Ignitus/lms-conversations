b8af40fecc53c630dfa8566dee7bbc4294b41102
dojo <sharadcodes>
2020-07-12 16:21:52 +0530

    Initial Commit

---
+++ src/helpers/routes/UserRoute.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
+import { isAuthenticated } from "../auth/auth_api_calls";
+
+const UserRoute = ({ component: Component, ...rest }) => {
+	return (
+		<Route
+			{...rest}
+			render={(props) =>
+				isAuthenticated() ? (
+					<Component {...props} />
+				) : (
+					<Redirect
+						to={{
+							pathname: "/signin",
+							state: { from: props.location },
+						}}
+					/>
+				)
+			}
+		/>
+	);
+};
+
+export default UserRoute;
---
+++ src/pages/common/Home.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Home = () => {
+	return <div>LMS Conversations</div>;
+};
+
+export default Home;
