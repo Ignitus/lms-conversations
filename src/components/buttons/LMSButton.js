99757d8be266529bf6f0c8045e5e7c06a80030c8
Sharad Raj <36638057+sharadcodes@users.noreply.github.com>
2020-07-13 18:31:52 +0530

    Added :rocket: Tailwind CSS and Button Components

--- src/components/buttons/LMSButton.js
+++ src/components/buttons/LMSButton.js
@@ -1,7 +1,70 @@
 import React from 'react';
+import './LMSButton.css';
 
-const LMSButton = (props) => {
-	return <button className="{props.class}">{props.title}</button>;
+const LMSButton = ({
+	title,
+	type,
+	size,
+	icon,
+	iconSize,
+	iconColor,
+	clickHandler,
+	loading,
+	...rest
+}) => {
+	let btnBaseClasses =
+		'font-bold py-2 px-4 rounded inline-flex items-center ';
+	let btnSizeClasses = '';
+	switch (size) {
+		case 'small':
+			btnSizeClasses += 'py-2 px-4';
+			break;
+		case 'large':
+			btnSizeClasses += 'py-3 px-6 text-lg';
+			break;
+		default:
+			btnSizeClasses = 'py-2 px-4';
+			break;
+	}
+
+	switch (type) {
+		case 'primary':
+			btnBaseClasses += 'bg-indigo-700 hover:bg-indigo-600 text-white ';
+			break;
+		case 'secondary':
+			btnBaseClasses += 'bg-gray-300 hover:bg-gray-400 text-gray-800 ';
+			break;
+		case 'success':
+			btnBaseClasses += 'bg-green-400 hover:bg-green-500 text-white ';
+			break;
+		case 'info':
+			btnBaseClasses += 'bg-blue-400 hover:bg-blue-500 text-white ';
+			break;
+		case 'warning':
+			btnBaseClasses += 'bg-orange-400 hover:bg-orange-500 text-white ';
+			break;
+		case 'danger':
+			btnBaseClasses += 'bg-red-400 hover:bg-red-500 text-white ';
+			break;
+		default:
+			btnBaseClasses += 'bg-gray-300 hover:bg-gray-400 text-gray-800 ';
+	}
+
+	return (
+		<button
+			className={'tracking-wide ' + btnBaseClasses + btnSizeClasses}
+			onClick={clickHandler}
+		>
+			{icon && (
+				<ion-icon
+					name={icon}
+                    size={iconSize ? iconSize : 'small'}
+                    class={loading ? 'rotating' : ''}
+				></ion-icon>
+			)}
+			<span className={icon ? 'ml-2' : ''}>{title}</span>
+		</button>
+	);
 };
 
 export default LMSButton;
--- src/pages/common/Home.js
+++ src/pages/common/Home.js
@@ -1,7 +1,45 @@
 import React from 'react';
+import LMSButton from '../../components/buttons/LMSButton';
 
 const Home = () => {
-	return <div>LMS Conversations</div>;
+	return (
+		<div>
+            <h3 className="my-2">Buttons</h3>
+
+			<LMSButton title="Log in" type="primary" size="small" clickHandler={(e)=> alert("Clicked")}/>
+			<LMSButton title="Log in" type="secondary" size="small" />
+			<LMSButton title="Log in" type="success" size="small" />
+			<LMSButton title="Log in" type="danger" size="small" />
+			<LMSButton title="Log in" type="info" size="small" />
+			<LMSButton title="Log in" type="warning" size="small" />
+			
+			<h3 className="my-2">With Icons</h3>
+			
+			<LMSButton title="Log in" type="primary" size="small" icon="information-circle" iconSize="large"/>
+			<LMSButton title="Log in" type="secondary" size="small" icon="information-circle" iconSize="large"/>
+			<LMSButton title="Log in" type="success" size="small" icon="information-circle" iconSize="large"/>
+			<LMSButton title="Log in" type="danger" size="small" icon="information-circle" iconSize="large"/>
+			<LMSButton title="Log in" type="info" size="small" icon="information-circle" iconSize="large"/>
+			<LMSButton title="Log in" type="warning" size="small" icon="information-circle" iconSize="large"/>
+
+            <h3 className="my-2">Large Buttons</h3>
+			
+			<LMSButton title="Log in" type="primary" size="large" icon="log-in" iconSize="large"/>
+			<LMSButton title="Log in" type="secondary" size="large" icon="log-in" iconSize="large"/>
+			<LMSButton title="Log in" type="success" size="large" icon="log-in" iconSize="large"/>
+			<LMSButton title="Log in" type="danger" size="large" icon="log-in" iconSize="large"/>
+			<LMSButton title="Log in" type="info" size="large" icon="log-in" iconSize="large"/>
+			<LMSButton title="Log in" type="warning" size="large" icon="log-in" iconSize="large"/>
+
+            <h3 className="my-2">Loading Buttons</h3>
+
+            <LMSButton title="Loading" type="primary" size="small" icon="reload-circle" iconSize="large" loading={true}/>
+            
+            <h3 className="my-2">Buttons with clickHandler</h3>
+
+            <LMSButton title="Click me" type="primary" size="small" clickHandler={()=> alert("Clicked")}/>
+		</div>
+	);
 };
 
 export default Home;
